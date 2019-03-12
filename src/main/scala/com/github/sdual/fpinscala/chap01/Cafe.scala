aca391a99be224058473f3def76564e444191ec8
sdual <shr.nrht.0623@gmail.com>
2019-03-13 01:52:01 +0900

    chap01

---
+++ src/main/scala/com/github/sdual/fpinscala/chap01/Cafe.scala
@@ -0,0 +1,44 @@
+package com.github.sdual.fpinscala.chap01
+
+case class CreditCard()
+
+class Cafe {
+
+  def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
+    val cup = new Coffee()
+    (cup, Charge(cc, cup.price))
+  }
+
+  def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) = {
+    val purchases: List[(Coffee, Charge)] = List.fill(n)(buyCoffee(cc))
+    val (coffees, charges) = purchases.unzip
+    (coffees, charges.reduce((c1, c2) => c1.combine(c2)))
+  }
+
+  def coalesce(charges: List[Charge]): List[Charge] =
+    charges.groupBy(_.cc).values.map(_.reduce(_ combine _)).toList
+
+}
+
+case class Charge(cc: CreditCard, amount: Int) {
+  def combine(other: Charge): Charge = {
+    if (cc == other.cc)
+      Charge(cc, amount + other.amount)
+    else
+      throw new Exception("Can't combine charges to different cards.")
+  }
+}
+
+class Coffee {
+
+  val price: Int = ???
+
+}
+
+class Payments {
+
+  def charge(cc: CreditCard, price: Int): Unit = {
+    ???
+  }
+
+}
