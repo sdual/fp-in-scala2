17b724850af0ec6b0bedecc5e0712ebb5730539a
sdual <shr.nrht.0623@gmail.com>
2020-05-23 23:46:41 +0900

    Exercise 3.28

--- src/main/scala/com/github/sdual/fpinscala/chap03/Tree.scala
+++ src/main/scala/com/github/sdual/fpinscala/chap03/Tree.scala
@@ -7,12 +7,25 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
+  // Exercise 3.28
+  def map[A, B](tree: Tree[A], f: A => B): Tree[B] = {
+
+    def loop(tr: Tree[A]): Tree[B] = {
+      tr match {
+        case Branch(l, r) => Branch(loop(l), loop(r))
+        case Leaf(v) => Leaf(f(v))
+      }
+    }
+
+    loop(tree)
+  }
+
   // Exercise 3.27
   def depth[A](tree: Tree[A]): Int = {
     def loop(tr: Tree[A], d: Int): Int = {
       tr match {
         case Branch(l, r) => loop(l, d + 1) max loop(r, d + 1)
-        case Leaf(v) => d + 1
+        case Leaf(_) => d + 1
       }
     }
 
@@ -38,7 +51,7 @@ object Tree {
     def loop(tr: Tree[A]): Int = {
       tr match {
         case Branch(l, r) => loop(l) + loop(r)
-        case Leaf(v) => 1
+        case Leaf(_) => 1
       }
     }
 
